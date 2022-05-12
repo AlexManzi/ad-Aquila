b7d261c30f62304033c419e009f6cb1d06f7e336
Alex Manzi <alex.manzi22@gmail.com>
2022-05-12 13:11:10 -0400

    removed route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
-    def fallback_index_html
-        render :file => 'public/index.html'
-    end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,6 @@ Rails.application.routes.draw do
   get "/clientReservations", to:"clients#showReservationsForClient"
   post "/create-checkout-session", to: "planpacks#add_plan"
 
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-  !request.xhr? && request.format.html?
-end
 
   # Defines the root path route ("/")
   # root "articles#index"
